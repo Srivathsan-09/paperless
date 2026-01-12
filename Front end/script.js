d0fa35953c5b23f66e384d562316d9585703cb25
Srivathsan N <srivathsan0906@gmail.com>
2026-01-13 01:39:13 +0530

    Revert "reverted to change"
    This reverts commit 49c7a4dcdfcef75c8d708a692aa1e735d7c004cf.

--- Front end/script.js
+++ Front end/script.js
@@ -2,6 +2,8 @@
  * Paperless Core Logic
  */
 
+const API_BASE_URL = 'https://paperless-rdsq.onrender.com';
+
 const STATE = {
     isLoggedIn: false,
     milkChart: null,
@@ -406,7 +408,7 @@ async function fetchAPI(url, options = {}) {
         headers['Authorization'] = `Bearer ${token}`;
     }
 
-    const response = await fetch(url, { ...options, headers });
+    const response = await fetch(`${API_BASE_URL}${url}`, { ...options, headers });
 
     if (response.status === 401) {
         localStorage.removeItem('authToken');
@@ -482,7 +484,7 @@ function checkAuth() {
 }
 
 const currentPage = window.location.pathname.split('/').pop() || 'index.html';
-const isAuthPage = currentPage === 'Loginpage.html' || currentPage === 'index.html';
+const isAuthPage = currentPage === 'index.html' || currentPage === '';
 
 
 if (!checkAuth() && !isAuthPage) {
